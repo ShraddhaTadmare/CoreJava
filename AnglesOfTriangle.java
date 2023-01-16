3dffc5db66e4caffcb1e0fd151ed837a450a832f
ShraddhaT <33415303+ShraddhaTadmare@users.noreply.github.com>
2023-01-16 22:03:31 +0530

    Add files via upload

---
+++ AnglesOfTriangle.java
@@ -0,0 +1,14 @@
+public class AnglesOfTriangle {
+
+	public static void main(String[] args) {
+		int p = 39;
+        int q = 89;
+        int r;
+
+        r = 180 - (p + q);
+
+        
+        System.out.println("\nThird angle of the triangle is = " + r );
+	}
+
+}
